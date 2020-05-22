5f3dbeada1ef14eb38bfa67fc2d6dfe3b8fae7f7
el-chuck <be.pickenbrock@gmail.com>
2020-05-22 22:05:03 +0200

    add intro message

---
+++ ue-triplex.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Your are standing in front of the castle gates and want to get in.";
+    std::cout << std::endl;
+    std::cout << "A guard asks you for the secret.";
+}
