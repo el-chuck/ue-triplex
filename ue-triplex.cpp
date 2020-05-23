86b1264ffcffe950e04ea63cc18f6a6a6145c2c1
el-chuck <be.pickenbrock@gmail.com>
2020-05-23 11:36:46 +0200

    refactor

--- ue-triplex.cpp
+++ ue-triplex.cpp
@@ -2,19 +2,21 @@
 
 int main()
 {
-    std::cout << "Your are standing in front of the castle gates and want to get in.";
+    // Print welcome message
+    std::cout << "Your are standing in front of the castle gates and want to get in." << std:: endl;
+    std::cout << "A guard asks you for the secret." << std::endl;
     std::cout << std::endl;
-    std::cout << "A guard asks you for the secret.";
-    std::cout << std::endl;
 
-    int a = 4;
-    int b = 7;
-    int c = 8;
+    // Declare code variables
+    int CodeA = 4;
+    int CodeB = 7;
+    int CodeC = 8;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+    int CodeSum = CodeA + CodeB + CodeC;
+    int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << "The secret consists out of three numbers...";
-    std::cout << "Their sum is: " << sum << std::endl;
-    std::cout << "Their product is: " << product << std::endl;
+    // Print code hints
+    std::cout << "The secret consists out of three numbers..." << std::endl;
+    std::cout << "Their sum is: " << CodeSum << std::endl;
+    std::cout << "Their product is: " << CodeProduct << std::endl;
 }
