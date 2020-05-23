3bd4b9bf951df6f1b279222b41802d00d2c33da2
el-chuck <be.pickenbrock@gmail.com>
2020-05-23 10:56:50 +0200

    task: print variable as new line

--- ue-triplex.cpp
+++ ue-triplex.cpp
@@ -5,4 +5,7 @@ int main()
     std::cout << "Your are standing in front of the castle gates and want to get in.";
     std::cout << std::endl;
     std::cout << "A guard asks you for the secret.";
+
+    int a = 4;
+    std::cout << std::endl << a;
 }
