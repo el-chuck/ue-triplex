4082beb2faceb88eb906998144f067f9e9ba1ee7
el-chuck <be.pickenbrock@gmail.com>
2020-05-23 11:03:00 +0200

    task: print sum and product

--- ue-triplex.cpp
+++ ue-triplex.cpp
@@ -5,10 +5,15 @@ int main()
     std::cout << "Your are standing in front of the castle gates and want to get in.";
     std::cout << std::endl;
     std::cout << "A guard asks you for the secret.";
+    std::cout << std::endl;
 
     int a = 4;
     int b = 7;
     int c = 8;
 
-    std::cout << std::endl << a;
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << "The sum is: " << sum << std::endl;
+    std::cout << "The product is: " << product << std::endl;
 }
