ca95ffde4d6550c0e840eaf3e89ca7a0ee923b09
el-chuck <be.pickenbrock@gmail.com>
2020-05-27 20:24:18 +0200

    extract functions and add ascii art

--- ue-triplex.cpp
+++ ue-triplex.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
-int main()
+void PrintIntro() 
 {
-    // Print welcome message
-    std::cout << "Your are standing in front of the castle gates and want to get in." << std:: endl;
+    std::cout <<
+        "                                  |>>>\n" <<
+        "                                  |\n" <<
+        "                    |>>>      _  _|_  _         |>>>\n" <<
+        "                    |        |;| |;| |;|        |\n" <<
+        "                _  _|_  _    \\.    .  /    _  _|_  _\n" <<
+        "               |;|_|;|_|;|    \\:. ,  /    |;|_|;|_|;|\n" <<
+        "               \\..      /    ||;   . |    \\.    .  /\n" <<
+        "                \\.  ,  /     ||:  .  |     \\:  .  /\n" <<
+        "                 ||:   |_   _ ||_ . _ | _   _||:   |\n" <<
+        "                 ||:  .|||_|;|_|;|_|;|_|;|_|;||:.  |\n" <<
+        "                 ||:   ||.    .     .      . ||:  .|\n" <<
+        "                 ||: . || .     . .   .  ,   ||:   |       \\,/\n" <<
+        "                 ||:   ||:  ,  _______   .   ||: , |            /`\\\n" <<
+        "                 ||:   || .   /+++++++\    . ||:   |\n" <<
+        "                 ||:   ||.    |+++++++| .    ||: . |\n" <<
+        "              __ ||: . ||: ,  |+++++++|.  . _||_   |\n" <<
+        "     ____--`~    '--~~__|.    |+++++__|----~    ~`---,              ___\n" <<
+        "-~--~                   ~---__|,--~'                  ~~----_____-~'   `~----~~\n\n\n";
+    std::cout << "Your are standing in front of the castle gates and want to get in." << std::endl;
     std::cout << "A guard asks you for the secret." << std::endl;
     std::cout << std::endl;
+}
+
+void PlayGame() 
+{
+    PrintIntro();
 
     // Declare code variables
     int CodeA = 4;
@@ -16,10 +39,9 @@ int main()
     int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print code hints
-    std::cout << "The secret consists out of three numbers..." << std::endl;
+    std::cout << "The secret consists out of three numbers...\n";
     std::cout << "Their sum is: " << CodeSum << std::endl;
-    std::cout << "Their product is: " << CodeProduct << std::endl;
-    std::cout << std::endl;
+    std::cout << "Their product is: " << CodeProduct << std::endl << std::endl;
 
     int GuessA, GuessB, GuessC;
     std::cout << "What do you reply to the guard?: ";
@@ -29,20 +51,23 @@ int main()
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
-    
+
     std::cout << "You entered: " << GuessA << " " << GuessB << " " << GuessC << std::endl;
     std::cout << "Your sum is: " << GuessSum << std::endl;
-    std::cout << "Your product is: " << GuessProduct << std::endl;
-    std::cout << std::endl;
+    std::cout << "Your product is: " << GuessProduct << std::endl << std::endl;
 
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
         std::cout << "The guard nods and lets you pass. Well done!" << std::endl;
     }
-    else 
+    else
     {
         std::cout << "The guard raises his pike and signals you to move away" << std::endl;
     }
+}
 
+int main()
+{
+    PlayGame();
     return 0;
 }
